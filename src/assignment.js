5091266965cc6462f8d64e0f3af1b2dcae8e945d
Tobenna Onyemeh <104394421+Tobenna96@users.noreply.github.com>
2022-07-16 00:48:53 +0000

    Add solution

--- src/assignment.js
+++ src/assignment.js
@@ -5,20 +5,20 @@ const assignment = {};
 
 
 
-function sumOfNumbers(arrayOfNumbers) {
-  let i = 0;
+function sumOfNumbers() {
   let sum = 0;
-
-  for (i = 0; i < arrayOfNumbers.length; i++) {
-    sum += arrayOfNumbers[i];
+let arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
   return sum;
 }
-sumOfNumbers()
+sumOfNumbers();
 
-function celsiusToFahrenheit(arr) {
+function celsiusToFahrenheit() {
   
   let fahrenheit = 0
+  let arr = [];
   let result = 0
   for (let i = 0; i<arr.length; i++){
     fahrenheit = arr[i] * (9/5) + 32
@@ -26,7 +26,7 @@ function celsiusToFahrenheit(arr) {
   } 
   return result;
 }
-celsiusToFahrenheit()
+celsiusToFahrenheit();
 
 
 // ========================
